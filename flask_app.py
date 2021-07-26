f6b59e6e67e8cae99eca83e205d823db647f2c23
Rafal Zebrowski <rafal.zebrowski@gmail.com>
2021-07-26 16:13:33 +0200

    flask speech recognition

---
+++ flask_app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, redirect, request
+import speech_recognition as sr
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    # atext- audio text, transcript of an audio file
+    atext = ''
+    
+    if request.method == 'POST':
+        print('data recived')
+        
+        if 'file-1' not in request.files:
+            return redirect(request.url)
+        else:
+            # afile- audio file
+            afile = request.files['file-1']
+            # print(type(afile))
+            # print(type(request.files))
+            # print(request.files)
+        
+        if afile.filename == '':
+            return redirect(request.url)
+        
+        if afile:
+            try:
+                r = sr.Recognizer()
+
+                with sr.AudioFile(afile) as source:
+                    adata = r.record(source)
+                atext = r.recognize_google(adata)
+            
+            except ValueError:
+                return redirect(request.url)
+
+    return render_template('index.html', transcript=atext)
+
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, threaded=True)
